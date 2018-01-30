c32a9149390d9c89776b26392ac51a1ec046c5d2
Stefano <stefano.dipaola@mindedsecurity.com>
2018-01-30 20:06:55 +0100

    added test for taggedTemplate call

--- tests/tests_OK/expected_acc/tagged_template_expression.js
+++ tests/tests_OK/expected_acc/tagged_template_expression.js
@@ -1,3 +1,7 @@
 e = 'e';
 l = 'l';
 setTimeout`a${ 'le' }rt`;
+a = x => {
+    return String.fromCharCode`${ 41 }`;
+};
+b = ')';
--- tests/tests_OK/tagged_template_expression.js
+++ tests/tests_OK/tagged_template_expression.js
@@ -1,3 +1,5 @@
 e = 'e';
 l = 'l';
 setTimeout`\x61${l+e}rt`;
+a=(x) => {return String.fromCharCode`${x}`};
+b=a(41);
